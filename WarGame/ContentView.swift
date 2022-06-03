fca589ff3bbbf456cb70721fcdec86e70d9379e8
Joe Khosbayar <joekhosbayar123@gmail.com>
2022-06-03 15:39:48 -0400

    Initial Commit

---
+++ WarGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WarGame
+//
+//  Created by Joe Khosbayar on 6/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WarGame/WarGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarGameApp.swift
+//  WarGame
+//
+//  Created by Joe Khosbayar on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct WarGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
